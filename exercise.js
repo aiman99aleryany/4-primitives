59ada31b923d06cd047ea907d1ff99abaf9be80a
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 03:23:55 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,10 @@
+// # Exercise 4
+
+// * Create the variables `firstName`, `lastName` and `fullName`.
+// * Assign them a value and print in the console the full name
+
+let firstName = "Aiman";
+let lastName = "Al Eryany";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
